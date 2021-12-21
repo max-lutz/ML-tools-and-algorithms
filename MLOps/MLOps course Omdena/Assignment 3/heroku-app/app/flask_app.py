51434fe9697d3e97b0512769637fd776a6b491d5
max-lutz <max.lutz@edu.ece.fr>
2021-12-21 12:39:28 -0600

    create simple app to run on heroku

---
+++ MLOps/MLOps course Omdena/Assignment 3/heroku-app/app/flask_app.py
@@ -0,0 +1,20 @@
+from app.functions import acres
+from flask import Flask, request
+
+#app
+app = Flask('__name__')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_input():
+    """
+    A flask app to take input and invoke a simple interest module
+    """
+    packet = request.get_json(force=True)
+    print(packet)
+    length = packet['length']
+    width = packet['width']
+
+    acres_ = acres(length, width)
+
+    return {"Acres":acres_}
